c5f0bc892032b6900bb347844a57618c8d3cb2bb
sourabhpande532 <sourabhpande43@gmail.com>
2026-01-12 11:45:38 +0530

    add new feature to cart and pl

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,23 +1,34 @@
-import { useCart } from "../context/CartContext"
+import { useCart } from "../context/CartContext";
 
 export const Cart = () => {
-    const { cart } = useCart();
-    return (
+  const { cart } = useCart();
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      {cart.length > 0 ? (
         <div>
-            <h1>Your Cart</h1>
-            { cart.length > 0 ? (
-                <div>
-                    { cart.map( ( item ) => {
-                        return <section style={{border:"2px solid black", padding:"20px", marginBottom:"15px"}} key={ item.id }>
-                            <h1 className="display-3">{ item.name }</h1>
-                            <h2>{ item.price }</h2>
-                            <p>{ item.details }</p>
-                        </section>
-                    } ) }
-                </div>
-            ) : ( <>
-                <h2>Your Cart is empty</h2>
-            </> ) }
+          {cart.map((item) => {
+            return (
+              <section
+                style={{
+                  border: "2px solid black",
+                  padding: "20px",
+                  marginBottom: "15px",
+                }}
+                key={item.id}
+                className='card p-2 shadow'>
+                <h1 className='display-3'>{item.name}</h1>
+                <h2>{item.price}</h2>
+                <p>{item.details}</p>
+              </section>
+            );
+          })}
         </div>
-    )
-}
+      ) : (
+        <>
+          <h2>Your Cart is empty</h2>
+        </>
+      )}
+    </div>
+  );
+};
